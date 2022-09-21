d7131205ab4044cc1a74e330c603f38f523e5e68
isorokina <93764917+isorokina@users.noreply.github.com>
2022-09-21 15:02:52 +0300

    lab2

--- students.py
+++ students.py
@@ -1,14 +1,24 @@
 class Student:
     
-    def __init__(self, name, marks):
+    def check_pass_fail(self):
+        if self.marks>=40:
+            return True
+        else:
+            return True
+
+    '''def __init__(self, name, marks):
         self.name=name
         self.marks=marks
 
-    def print(self, name, marks):
-        print(self.name, self.marks)
+    def izdruka(self, name, marks):
+        print(self.name, self.marks)'''
 
-student1=Student('Tom', 45)
-#student1.print
+   
+student1=Student()
+student1.name='Tom'
+student1.marks=50
+did_pass=student1.check_pass_fail
+print(did_pass)
 #print(student1.name, student1.marks)
 
 
